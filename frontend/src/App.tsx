b3cfd3e58cca8653918fd88481a2650c2ec994ca
idiot-kitto <46341496+idiot-kitto@users.noreply.github.com>
2021-12-23 19:44:47 +0900

    feat: rain Animation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
+const App = () => {
+  const amount = 150;
+  const body = document.querySelector("body");
+  let i = 0;
+  while (i < amount) {
+    let drop = document.createElement("i");
+    let size = Math.random() * 5;
+    let posX = Math.floor(Math.random() * window.innerWidth);
+    let delay = Math.random() * -20;
+    let duration = Math.random() * 5;
 
-    </div>
-  );
-}
+    drop.style.width = 0.2 + size + "px";
+    drop.style.left = posX + "px";
+    drop.style.animationDelay = delay + "s";
+    drop.style.animationDuration = 1 + duration + "s";
+    body?.appendChild(drop);
+    i++;
+  }
+
+  return <div></div>;
+};
 
 export default App;
