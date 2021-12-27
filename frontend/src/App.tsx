0282da8a9013e5bb88dd42a4235739aa6ae97229
idiot-kitto <46341496+idiot-kitto@users.noreply.github.com>
2021-12-27 17:38:02 +0900

    feat: recoil modal 상태 관리

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,25 @@
 import React from "react";
+import { RecoilRoot } from "recoil";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import AlertModal from "./components/AlertModal";
 import { Home, Rain, Cube, Loading, Button } from "./pages";
 
 const App = () => {
   return (
-    <Router>
-      <Home />
-      <Routes>
-        <Route path="/" element={null} />
-        <Route path="/rain" element={<Rain />} />
-        <Route path="/cube" element={<Cube />} />
-        <Route path="/loading" element={<Loading />} />
-        <Route path="/button" element={<Button />} />
-      </Routes>
-    </Router>
+    <RecoilRoot>
+      <AlertModal />
+      <Router>
+        <Home />
+        <Routes>
+          <Route path="/" element={null} />
+          <Route path="/rain" element={<Rain />} />
+          <Route path="/cube" element={<Cube />} />
+          <Route path="/loading" element={<Loading />} />
+          <Route path="/button" element={<Button />} />
+        </Routes>
+      </Router>
+    </RecoilRoot>
   );
 };
 
